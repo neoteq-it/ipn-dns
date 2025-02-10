e5220601c1550a044245491eec0467e8ac21ed4c
Jean Stickelmann <67966685+underfake@users.noreply.github.com>
2025-02-10 12:18:09 +0100

    add neoteq 4via6

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/coredns/coredns/core/plugin"
 	"github.com/coredns/coredns/coremain"
 	_ "github.com/damomurf/coredns-tailscale"
+	_ "github.com/neoteq-it/neoteqts4via6"
 	_ "github.com/coredns/records"
 )
 
